0d2cb4381e32dbdee089d78729a55902f4c99082
N1ghtMoon <1076751236@qq.com>
2018-10-07 06:20:51 +0800

    1.01

---
+++ header.lua
@@ -0,0 +1,16 @@
+return {
+  id = "MoonSeriesRW",
+  name = "MoonSeries",
+  riot = true,
+  flag = {
+    text = 'MoonSeries',
+    color = { 
+      text = 0xffeeeeee, 
+      background1 = 0x66f77474, 
+      background2 = 0x99000000, 
+    },
+  },
+  load = function()
+    return true
+  end,
+}
