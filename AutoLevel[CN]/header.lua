2ae10eaf6699bf1691af3f4528212e333cf08e54
N1ghtMoon <1076751236@qq.com>
2018-10-18 05:50:07 +0800

    Add AutoLevel (CN Server Only)

---
+++ AutoLevel[CN]/header.lua
@@ -0,0 +1,17 @@
+-- Moon - 自动加点
+
+return {
+  id = "MoonAutoLevel",
+  name = "Moon - 自动加点",
+  flag = {
+    text = 'Moon - 自动加点',
+    color = { 
+      text = 0xffeeeeee, 
+      background1 = 0x66f77474, 
+      background2 = 0x99000000, 
+    },
+  },
+  load = function()
+    return true
+  end,
+}
