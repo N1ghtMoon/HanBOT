57e75d547a49ded075df286e8d3208b7d881379b
N1ghtMoon <1076751236@qq.com>
2018-12-20 05:34:57 +0800

    ty

---
+++ MoonWalker V2/header.lua
@@ -0,0 +1,16 @@
+return {
+  id = "MoonWalkerV2",
+  name = "MoonWalker V2",
+  riot = true,
+  flag = {
+    text = 'NightMoon',
+    color = { 
+      text = 0xffeeeeee, 
+      background1 = 0x66f77474, 
+      background2 = 0x99000000, 
+    },
+  },
+  load = function()
+    return true
+  end,
+}
